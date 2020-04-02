c9df244e5e0c842737ae947c6cbcd1dca5144c7e
Myron <dubtwee@gmail.com>
2020-04-02 16:39:25 -0500

    serving up static files for web

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static('public'));
+
 app.listen(PORT, () => {
   console.log('Server is running on PORT:', PORT);
 });
